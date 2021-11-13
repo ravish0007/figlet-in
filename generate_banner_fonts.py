9720e728d704416db51ee44883ff9c3da7b9faea
ravish0007 <ravish.nfs@gmail.com>
2021-11-13 11:31:00 +0000

    Add banner fonts

---
+++ generate_banner_fonts.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+
+def call_jp2a(image_dir, output_dir, width=30):
+    
+    for image in os.listdir(image_dir):
+        code, ext = os.path.splitext(image)
+        with open( os.path.join(output_dir, f'{code}.txt'), "w") as outfile:
+            subprocess.Popen(['jp2a', f'--width={width}', '--chars= #', os.path.join(image_dir,image)], stdout=outfile)      # hack around with chars
+
+
+
+if __name__ == "__main__":
+
+    image_dir = "fonts"
+    output_dir = "banner_text"
+    
+    os.makedirs(output_dir, exist_ok=True)
+    call_jp2a(image_dir, output_dir)
--- notes.md
+++ notes.md
@@ -15,5 +15,11 @@ India has even more and will be focused later because of time ;(
 Using vips to generate image for each chars, and converting via jp2a, 
 https://github.com/dijkstracula/Asciimatic  license missing but failing to compile.
 
+jp2a is amazing because of chars intake.
+generating banner like is quite done
 
+
+hacking with figlet is quite pending also quite daunting, few days ago had to install window7, xp64, xp32 just to see figed working.
+
+README in figlet source is the only way to understand font format
 
